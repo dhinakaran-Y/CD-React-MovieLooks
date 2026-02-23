dc535d6ba7f86fe12a434b12c0504f50f0958e24
dhinakaran-Y <dhinakaran.y.dhina14@gmail.com>
2026-02-23 10:04:03 +0530

    fix: In vite.config.js -- task-3 rollupAction added

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
-      input: ["index.html", "task-2.html"],
+      input: ["index.html", "task-2.html", "task-3.html"],
     },
   },
 });
